6263aadd9b77bd0f59b6e23b07dc3fabb304465d
Auwalu Aminu <aaagumel12@gmail.com>
2025-08-17 13:08:58 +0100

    Activate Time with initial minutes as 40 minutes

--- app/src/main/java/com/trymaster/QuizViewActivity.java
+++ app/src/main/java/com/trymaster/QuizViewActivity.java
@@ -4,15 +4,27 @@ import android.os.Bundle;
 import com.trymaster.database.Question;
 import java.util.List;
 import java.util.ArrayList;
+import android.widget.TextView;
+import android.os.Handler;
+import android.widget.Button;
+import android.view.View.*;
+import android.view.*;
 
 public class QuizViewActivity extends AppCompatActivity
 {
 
 	List<Question> quiz;
+	TextView tv_time_sec,tv_time_min;
+	Button prev_button,next_button;
+	int second,minute;
+	
+//	Handler handler;
+//	Runnable runnable;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
+	
 		
 		quiz=new ArrayList<>();
 		quiz.add(new Question(1,"who is president of Nigeria","Bola","bukhari","Atiku","Rabiu","Bola"));
@@ -22,7 +34,60 @@ public class QuizViewActivity extends AppCompatActivity
 		// TODO: Implement this method
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.quiz_view);
+		second=60;
+		minute=40;
 
+		prev_button=findViewById(R.id.btn_prev);
+		next_button=findViewById(R.id.btn_next);
+		//Reverse Previous question to View
+		prev_button.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View p1)
+				{
+					// TODO: Implement this method
+				}
+		});
+		//Get Next Question on View
+		next_button.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View p1)
+				{
+					// TODO: Implement this method
+				}
+		});
+		
+		
+		tv_time_min=findViewById(R.id.tv_quiz_time_min);
+		tv_time_sec=findViewById(R.id.tv_quiz_time_sec);
+		final Handler handler = new Handler();
+		Runnable runnable = new Runnable() {
+			@Override
+			public void run() {
+				// Update your view here
+				if(minute == 0){
+					//Toast.makeText(this.getContext(),"Time up",100).show();
+					handler.removeCallbacks(this);
+				}
+				updateView();
+				handler.postDelayed(this, 1000); // 1000ms = 1s
+			}
+		};
+		handler.post(runnable);
+
+	}
+	
+	public void  updateView(){
+		//update quiz time text view
+		if(second == 0){
+			minute=minute-1;
+			second=60;
+		}
+		
+		second=second-1;
+		String s=Integer.toString(second);
+		tv_time_sec.setText(s);
+		tv_time_min.setText(Integer.toString(minute));
+		
 	}
 
 }
